2d7c0493203366d498977949b72a9d6f8dc4a0a2
Lino116 <linomoreau2@gmail.com>
2021-05-31 18:00:43 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -93,6 +93,12 @@ class App extends Component {
             }
             style={{ display: "inline" }}
           >
+            <span
+              className="iconify language-icon mr-5"
+              data-icon="twemoji-flag-for-flag-united-kingdom"
+              data-inline="false"
+              id={window.$primaryLanguageIconId}
+            ></span>
           </div>
           <div
             onClick={() =>
@@ -103,6 +109,12 @@ class App extends Component {
             }
             style={{ display: "inline" }}
           >
+            <span
+              className="iconify language-icon"
+              data-icon="twemoji-flag-for-flag-poland"
+              data-inline="false"
+              id={window.$secondaryLanguageIconId}
+            ></span>
           </div>
         </div>
         <About
