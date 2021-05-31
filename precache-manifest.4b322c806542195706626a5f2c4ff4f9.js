d75c605f53c4e3f75cef17ff95ff17e5aa874c35
Lino116 <linomoreau2@gmail.com>
2021-05-31 18:26:47 +0200

    Updates

--- precache-manifest.4b322c806542195706626a5f2c4ff4f9.js
+++ precache-manifest.4b322c806542195706626a5f2c4ff4f9.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f61856b5497745b24845d19d1d47defc",
+    "revision": "fa6b66053523fa2c1fd272666313f659",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.d64471e2.chunk.css"
   },
   {
-    "revision": "74a38f169e8874223714",
+    "revision": "729588d81fbd74dfde9d",
     "url": "/static/css/main.40f0e6cc.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.88cb3c1e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "74a38f169e8874223714",
-    "url": "/static/js/main.4d9741b8.chunk.js"
+    "revision": "729588d81fbd74dfde9d",
+    "url": "/static/js/main.13d68dc0.chunk.js"
   },
   {
     "revision": "a2d41c56873c741b51b2",
