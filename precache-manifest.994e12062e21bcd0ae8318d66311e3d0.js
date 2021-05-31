1ee65f5c5f9c200517f068aec48677a1f5f51d98
Lino116 <linomoreau2@gmail.com>
2021-05-31 18:20:44 +0200

    Updates

--- precache-manifest.994e12062e21bcd0ae8318d66311e3d0.js
+++ precache-manifest.994e12062e21bcd0ae8318d66311e3d0.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "092c265d7a2d8cf9dd53fa754e0c1e40",
+    "revision": "f61856b5497745b24845d19d1d47defc",
     "url": "/index.html"
   },
   {
-    "revision": "384b6a27700e41f5abd7",
+    "revision": "58b33c836e144ca2b6f7",
     "url": "/static/css/2.d64471e2.chunk.css"
   },
   {
-    "revision": "47e26e6d75e5166ae805",
+    "revision": "74a38f169e8874223714",
     "url": "/static/css/main.40f0e6cc.chunk.css"
   },
   {
-    "revision": "384b6a27700e41f5abd7",
-    "url": "/static/js/2.a42c43c6.chunk.js"
+    "revision": "58b33c836e144ca2b6f7",
+    "url": "/static/js/2.88cb3c1e.chunk.js"
   },
   {
     "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
-    "url": "/static/js/2.a42c43c6.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.88cb3c1e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "47e26e6d75e5166ae805",
-    "url": "/static/js/main.2ca8c6d8.chunk.js"
+    "revision": "74a38f169e8874223714",
+    "url": "/static/js/main.4d9741b8.chunk.js"
   },
   {
     "revision": "a2d41c56873c741b51b2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.340f01b684db8fe2f876815e47f56141.js"
+  "/precache-manifest.994e12062e21bcd0ae8318d66311e3d0.js"
 );
 
 self.addEventListener('message', (event) => {
